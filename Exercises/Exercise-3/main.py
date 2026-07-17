0bf470c8fee8045e67f60ad50f87dfe9ef3e1fc3
Djim Schaap | Websteen <93537121+DjimSchaap@users.noreply.github.com>
2026-07-17 07:37:53 +0200

    complete exercise 3 using requests since boto3 unsigned is no longer allowed on common crawl

--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -1,9 +1,34 @@
-import boto3
+import gzip
+import io
+import os
+
+import requests
 
 
 def main():
-    # your code here
-    pass
+    baseUrl = "https://data.commoncrawl.org"
+    pathsKey = "crawl-data/CC-MAIN-2022-05/wet.paths.gz"
+
+    with requests.get(f"{baseUrl}/{pathsKey}", stream=True) as response:
+        response.raise_for_status()
+        response.raw.decode_content = True
+
+        with gzip.GzipFile(fileobj=response.raw) as file:
+            key = io.TextIOWrapper(file, encoding="utf-8").readline().strip()
+
+    localFile = os.path.basename(key)
+
+    with requests.get(f"{baseUrl}/{key}", stream=True) as response:
+        response.raise_for_status()
+
+        with open(localFile, mode="wb") as file:
+            for chunk in response.iter_content(chunk_size=1024 * 1024):
+                if chunk:
+                    file.write(chunk)
+
+    with gzip.open(localFile, mode="rt", encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")
 
 
 if __name__ == "__main__":
